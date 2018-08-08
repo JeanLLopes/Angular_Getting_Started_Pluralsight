127c09f45c55068b66fc37e347223e5d73b335b2
jlopes <jean.llopes@outlook.com>
2018-08-08 01:41:16 -0300

    class 12

--- APM-Start/src/app/products/products-detail.component.ts
+++ APM-Start/src/app/products/products-detail.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { IProduct } from './product';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   templateUrl: './products-detail.component.html',
@@ -8,9 +9,27 @@ import { IProduct } from './product';
 export class ProductsDetailComponent implements OnInit {
   pageTitle = 'Product Details';
   product: IProduct;
-  constructor() { }
+  constructor(private route: ActivatedRoute, private router: Router) { }
 
   ngOnInit() {
+    // '+'convert a string to number
+    const id: number = +this.route.snapshot.paramMap.get('id');
+
+    this.pageTitle += ': ' + id;
+    this.product = {
+      'productId': id,
+      'productName': 'Leaf Rake',
+      'productCode': 'GDN-0011',
+      'releaseDate': 'March 19, 2016',
+      'description': 'Leaf rake with 48-inch wooden handle.',
+      'price': 19.95,
+      'starRating': 3.2,
+      'imageUrl': 'https://openclipart.org/image/300px/svg_to_png/26215/Anonymous_Leaf_Rake.png'
+    };
+  }
+
+  onBack(): void {
+    this.router.navigate(['/products']);
   }
 
 }
